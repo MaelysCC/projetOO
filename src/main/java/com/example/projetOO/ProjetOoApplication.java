c3d0874331ec341db4560665e00c0e8af753e6bb
MaelysCC <maelys.de-crouy-chanel@efrei.net>
2026-09-11 09:00:52 +0200

    Initial commit

---
+++ src/main/java/com/example/projetOO/ProjetOoApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetOO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetOoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetOoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetOO/ProjetOoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetOO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetOoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
