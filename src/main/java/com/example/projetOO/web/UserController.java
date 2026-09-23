be78a9f340442260b3898d4a1c1215bf5c8ed5e8
MaelysCC <maelys.de-crouy-chanel@efrei.net>
2026-09-23 20:22:49 +0200

    controller user qui veut pas se faire send

--- src/main/java/com/example/projetOO/web/UserController.java
+++ src/main/java/com/example/projetOO/web/UserController.java
@@ -1,5 +1,45 @@
 package com.example.projetOO.web;
 
+import com.example.projetOO.entities.User;
+import com.example.projetOO.service.UserService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
-    
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user){
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long id){userService.deleteUser(id);}
 }
